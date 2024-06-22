543093f38394ddeb6d545be97383d4e4a687b6e9
Avinashkumar8576 <avinashkumar90089008@gmail.com>
2024-06-22 23:57:14 +0530

    first commit

---
+++ src/javaPrimitiveDataTypes/Example5.java
@@ -0,0 +1,31 @@
+package javaPrimitiveDataTypes;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+
+		int value = 45;
+		
+		
+		
+		//non-prmitive data types: arrays and strings
+		int values[] = {45, 67, 89, 90};
+		
+		char name = 'D';
+		
+		char myName[] = {'D', 'i', 'l', 'k', 'a', 's', 'h'};
+		
+		String friendName = "Dilkash";
+		
+		System.out.println(myName);
+		System.out.println(friendName.charAt(0));
+		
+		
+		
+		
+		
+
+	}
+
+}
+
